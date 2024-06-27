c36d6de3cc94435eff1f936518e87c2d21402549
Vitor Fonseca <vitorlofonseca@hotmail.com>
2024-06-27 18:38:33 +0100

    Element plus sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { CirclePlus } from '@element-plus/icons-vue'
+import SidebarContent from './SidebarContent.vue'
 import { ElDrawer } from 'element-plus'
 import { Menu as MenuIcon } from '@element-plus/icons-vue'
 import { ref } from 'vue'
@@ -21,21 +21,11 @@ const drawerIsOpened = ref(false)
 
 <template>
   <MenuIcon class="MenuIcon" @click="drawerIsOpened = true" />
-  <navbar class="Sidebar">
-    <div class="Sidebar__Header">
-      <img
-        class="Sidebar__Lettering"
-        src="@/assets/images/workana-lettering.png"
-        alt="Workana lettering logo"
-      />
-      <CirclePlus class="Sidebar__AddIcon" />
-    </div>
 
-    <h5 class="Sidebar__Content">Ainda não tem histórico de conversas :(</h5>
-  </navbar>
+  <SidebarContent class="Sidebar" />
 
-  <ElDrawer v-model="drawerIsOpened" title="I am the title" :direction="'ltr'" :size="drawerWidth">
-    <span>Hi, there!</span>
+  <ElDrawer v-model="drawerIsOpened" :direction="'ltr'" :size="drawerWidth">
+    <SidebarContent />
   </ElDrawer>
 </template>
 
@@ -51,44 +41,16 @@ const drawerIsOpened = ref(false)
 }
 
 .Sidebar {
-  background-color: lighten($color: $color-primary, $amount: 55);
-  flex-grow: 1;
-  max-width: 30vw;
-  padding: 30px;
-
-  &__Header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  &__Lettering {
-    height: 22px;
-  }
-
-  &__AddIcon {
-    width: 37px;
-    color: $color-primary;
-    cursor: pointer;
-  }
-
-  &__Content {
-    height: 80vh;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
+  display: block;
 }
 
 @media only screen and (max-width: 900px) {
-  .Sidebar {
-    display: none;
-  }
-
   .MenuIcon {
     display: block;
+  }
+
+  .Sidebar {
+    display: none;
   }
 }
 </style>
---
+++ frontend/src/components/SidebarContent.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { CirclePlus } from '@element-plus/icons-vue'
+</script>
+
+<template>
+  <div class="SidebarContent">
+    <div class="SidebarContent__Header">
+      <img
+        class="SidebarContent__Lettering"
+        src="@/assets/images/workana-lettering.png"
+        alt="Workana lettering logo"
+      />
+      <CirclePlus class="SidebarContent__AddIcon" />
+    </div>
+
+    <h5 class="SidebarContent__Content">Ainda não tem histórico de conversas :(</h5>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.SidebarContent {
+  background-color: lighten($color: $color-primary, $amount: 55);
+  padding: 30px;
+  width: 100%;
+  max-width: 30vw;
+
+  &__Header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__Lettering {
+    height: 22px;
+  }
+
+  &__AddIcon {
+    width: 37px;
+    color: $color-primary;
+    cursor: pointer;
+  }
+
+  &__Content {
+    height: 80vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+}
+
+@media only screen and (max-width: 900px) {
+  .SidebarContent {
+    max-width: unset;
+  }
+}
+</style>
